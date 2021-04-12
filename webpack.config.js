487524aaa0b33f9361aaf03d7c59a7fed12e8bda
Bogdan Puscasu <bogdan.puscasu@travelport.com>
2021-04-12 12:23:10 +0300

    project scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    clientLogLevel: 'silent',
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+ plugins: [new HtmlWebpackPlugin({ template: './public/index.html', favicon: './public/favicon.ico' })],
+}
