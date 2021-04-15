9d34107fe7ac6e1ab999e340bb056156daa8948d
Bogdan Puscasu <bogdan.puscasu@travelport.com>
2021-04-15 13:02:53 +0300

    init middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+
 import './index.css';
+import store from './store';
 import { App } from './components';
 
 if (module.hot) module.hot.accept()
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import {createStore, applyMiddleware, compose} from 'redux'
 
 import rootReducer from '../reducers';
+import { logger } from '../middlewares';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware()))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
